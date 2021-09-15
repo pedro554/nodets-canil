ba294a73d40503b4bbfdad393f52cdca823cd90e
pedro554 <63167281+pedro554@users.noreply.github.com>
2021-09-15 10:39:26 -0300

    Projeto Final 1.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,55 @@
 import {Request, Response} from 'express'
+import {createMenuObject} from '../helpers/createMenuObject'
+import {Pet} from '../models/pet'
 
 export const home = (req: Request, res: Response) => {
-    res.send('Home')
+    let list = Pet.getAll()
+
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg',
+        },
+        list
+    })
 }
 
 export const dogs = (req: Request, res: Response) => {
-    res.send('Dogs')
+    let list = Pet.getFromType('dog')
+
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Todos os cachorros',
+            background: 'banner_dog.jpg',
+        },
+        list
+    })
 }
 
 export const cats = (req: Request, res: Response) => {
-    res.send('Cats')
+    let list = Pet.getFromType('cat')
+
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Todos os gatos',
+            background: 'banner_cat.jpg',
+        },
+        list
+    })
 }
 
 export const fishes = (req: Request, res: Response) => {
-    res.send('Fishes')
+    let list = Pet.getFromType('fish')
+
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Todos os peixes',
+            background: 'banner_fish.jpg',
+        },
+        list
+    })
 }
